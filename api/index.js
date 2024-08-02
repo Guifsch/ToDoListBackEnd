67f84e46ac725d3db11041a8f01a2d83369d0aed
Guilherme de Freitas Schwingel <gdschwingel@inf.ufsm.br>
2024-08-02 15:03:24 -0300

    mudanças

--- api/index.js
+++ api/index.js
@@ -27,10 +27,10 @@ mongoose
     console.log(err, "ERROR");
   });
 const corsOptions = {
-  origin: ["https://guifsch.github.io"],
+  origin: ["https://gym-bro-frontend.vercel.app", "http://localhost:5173"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Incluindo OPTIONS
-  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"], // Adicione outros cabeçalhos se necessário
+  allowedHeaders: ["Content-Type", "Authorization"], // Adicione outros cabeçalhos se necessário
 };
 
 // Middleware de CORS
